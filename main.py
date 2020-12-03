1f653b8dda16c9ce68fc7998dfc7e7e29b4689e3
AnnaAGor <71843992+AnnaAGor@users.noreply.github.com>
2020-12-03 23:38:23 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ New_Docs = ['TheWarDoctor', 'NinthDoctor', 'TenthDoctor', 'EleventhDoctor', 'Twe
 New_Docs_About = [['John Hurt (50th anniversary episode) Although not technically classed as an official Doctor, The War Doctor\'s origin is explained in the mini-episode "The Night of the Doctor" whereby the eighth Doctor wishes to regenerate as a Warrior, instead of a Doctor. This is confirmed when he regenerates as John Hurt, whose first words are "Doctor no more." He then joins the tenth and eleventh Doctors in an attempt to stop the war on their home planet of Gallifrey.'], ['Christopher Eccleston (2005). The sole survivor of the Last Great Time War, scarred by the terrible things he\’d seen and done, the Ninth Doctor was an intense and emotional incarnation. He took Rose Tyler to see the end of the world, inspired Charles Dickens and showed that for once, everybody could live.'], ['David Tennant (2005–2010). Waking on Christmas Day in his new form, the tenth Doctor fought the Sycorax high above London. Travelling with Rose and Mickey he battled Cybermen, werewolves and possibly even the Devil itself. David was voted the nation\'s favourite Doctor.'], ['Matt Smith (2010–2013). Born, “still cooking”, into a crashing TARDIS, the Eleventh Doctor hurtled into the life of Amy Pond. The Doctor and Amy battled new paradigm Daleks in World War Two, Weeping Angels by the thousand and the depression in Vincent van Gogh\’s mind.'], ['Peter Capaldi (2013-2017). Peter first appeared as the Doctor briefly in the 50th anniversary special episode, but played minor characters in previous series. Peter\'s Doctor fought Daleks and Davros, Cybermen, Zygons, the Veil and the Time Lords.'], ['Jodie Whittaker (2017-present). We\'re yet to see what Jodie will be like as the Doctor. She\'s the first female to play the role and the first words she said when she realised she\'d regenerated as a woman were \'Aw, Brilliant!\' We agree!']]
 New_Doctors_About = dict(zip(New_Docs, New_Docs_About))
 
-New_Doctors =  {'New_Doctors_name': ['TheWarDoctor', 'NinthDoctor', 'TenthDoctor', 'EleventhDoctor', 'TwelfthDoctor', 'ThirteenthDoctor'],
+New_Doctors = {'New_Doctors_name': ['TheWarDoctor', 'NinthDoctor', 'TenthDoctor', 'EleventhDoctor', 'TwelfthDoctor', 'ThirteenthDoctor'],
                              'About':[['John Hurt (50th anniversary episode) Although not technically classed as an official Doctor, The War Doctor\'s origin is explained in the mini-episode "The Night of the Doctor" whereby the eighth Doctor wishes to regenerate as a Warrior, instead of a Doctor. This is confirmed when he regenerates as John Hurt, whose first words are "Doctor no more." He then joins the tenth and eleventh Doctors in an attempt to stop the war on their home planet of Gallifrey.'],
                                       ['Christopher Eccleston (2005). The sole survivor of the Last Great Time War, scarred by the terrible things he\’d seen and done, the Ninth Doctor was an intense and emotional incarnation. He took Rose Tyler to see the end of the world, inspired Charles Dickens and showed that for once, everybody could live.'],
                                       ['David Tennant (2005–2010). Waking on Christmas Day in his new form, the tenth Doctor fought the Sycorax high above London. Travelling with Rose and Mickey he battled Cybermen, werewolves and possibly even the Devil itself. David was voted the nation\'s favourite Doctor.'], ['Matt Smith (2010–2013). Born, “still cooking”, into a crashing TARDIS, the Eleventh Doctor hurtled into the life of Amy Pond. The Doctor and Amy battled new paradigm Daleks in World War Two, Weeping Angels by the thousand and the depression in Vincent van Gogh\’s mind.'],
@@ -99,7 +99,7 @@ def cmd_start(message):
                                       "Type /info to know what I am and what I can do for you.\n"
                                       "Type /commands to list the available commands.\n"
                                       "Type /reset to discard previous selections and start anew.")
-    bot.send_photo(message.chat.id, pict[randint(0, 8)])
+    bot.send_photo(message.chat.id, pict[randint(0, 7)])
     dbworker.set_state(message.chat.id, config.States.S_INTRODUCTION.value)
 
 @bot.message_handler(commands=["reset"])
@@ -160,11 +160,11 @@ def about_doctor(message):
         elif index == 3:
             bot.send_photo(message.chat.id, pict4[randint(0, 3)])
         elif index == 4:
-            bot.send_photo(message.chat.id, pict5[randint(0, 8)])
+            bot.send_photo(message.chat.id, pict5[randint(0, 7)])
         elif index == 5:
-            bot.send_photo(message.chat.id, pict6[randint(0, 6)])
+            bot.send_photo(message.chat.id, pict6[randint(0, 4)])
         elif index == 6:
-            bot.send_photo(message.chat.id, pict7[randint(0, 4)])
+            bot.send_photo(message.chat.id, pict7[randint(0, 3)])
         elif index == 7:
             bot.send_photo(message.chat.id, pict8[randint(0, 3)])
         dbworker.set_state(str(message.chat.id), n)
@@ -175,7 +175,7 @@ def about_doctor(message):
         if index == 0:
             bot.send_photo(message.chat.id, pictNo[randint(0, 3)])
         elif index == 1:
-            bot.send_photo(message.chat.id, pict9[randint(0, 2)])
+            bot.send_photo(message.chat.id, pict9[randint(0, 3)])
         elif index == 2:
             bot.send_photo(message.chat.id, pict10[randint(0, 4)])
         elif index == 3:
@@ -206,38 +206,38 @@ def about_doctor(message):
         bot.send_message(message.chat.id, Classic_Doctors_About[n])
         index = Classic_Docs.index(n)
         if index == 0:
-            bot.send_photo(message.chat.id, pict1[randint(0, 6)])
+            bot.send_photo(message.chat.id, pict1[randint(0, 5)])
         elif index == 1:
-            bot.send_photo(message.chat.id, pict2[randint(0, 3)])
+            bot.send_photo(message.chat.id, pict2[randint(0, 2)])
         elif index == 2:
             bot.send_photo(message.chat.id, pict3[randint(0, 3)])
         elif index == 3:
-            bot.send_photo(message.chat.id, pict4[randint(0, 3)])
+            bot.send_photo(message.chat.id, pict4[randint(0, 2)])
         elif index == 4:
-            bot.send_photo(message.chat.id, pict5[randint(0, 8)])
+            bot.send_photo(message.chat.id, pict5[randint(0, 7)])
         elif index == 5:
-            bot.send_photo(message.chat.id, pict6[randint(0, 6)])
+            bot.send_photo(message.chat.id, pict6[randint(0, 4)])
         elif index == 6:
-            bot.send_photo(message.chat.id, pict7[randint(0, 4)])
+            bot.send_photo(message.chat.id, pict7[randint(0, 3)])
         elif index == 7:
-            bot.send_photo(message.chat.id, pict8[randint(0, 3)])
+            bot.send_photo(message.chat.id, pict8[randint(0, 2)])
         dbworker.set_state(str(message.chat.id), n)
         dbworker.set_state(message.chat.id, config.States.S_ENTER_WHICH_DOCTOR.value)
     elif n in New_Docs:
         bot.send_message(message.chat.id, New_Doctors_About[n])
         index = New_Docs.index(n)
         if index == 0:
-            bot.send_photo(message.chat.id, pictNo[randint(0, 3)])
+            bot.send_photo(message.chat.id, pictNo[randint(0, 2)])
         elif index == 1:
-            bot.send_photo(message.chat.id, pict9[randint(0, 2)])
+            bot.send_photo(message.chat.id, pict9[randint(0, 3)])
         elif index == 2:
             bot.send_photo(message.chat.id, pict10[randint(0, 3)])
         elif index == 3:
-            bot.send_photo(message.chat.id, pict11[randint(0, 4)])
+            bot.send_photo(message.chat.id, pict11[randint(0, 3)])
         elif index == 4:
             bot.send_photo(message.chat.id, pict12[randint(0, 6)])
         elif index == 5:
-            bot.send_photo(message.chat.id, pict13[randint(0, 6)])
+            bot.send_photo(message.chat.id, pict13[randint(0, 4)])
         dbworker.set_state(str(message.chat.id), n)
         dbworker.set_state(message.chat.id, config.States.S_ENTER_WHICH_DOCTOR.value)
     else:
@@ -252,7 +252,7 @@ def cmd_commands(message):
     bot.send_message(message.chat.id, "/reset - is used to discard previous selections and start anew.\n"
                                       "/start - is used to start a new dialogue from the very beginning.\n"
                                       "/info - is used to know what i can do for you (there's a tree of commands)\n"
-                                      "/commands - If you got here, you know what it is used for.\n"
+                                      "/oldschool or /newera - for lists of Doctor's names \n"
                                       "/intro_docto_who - is used to list find basic info about.\n")
 
 pict1 = ['https://media.archonia.com/images/samples/30/80/283080_s0.jpg',
@@ -283,11 +283,9 @@ pict6 = ['https://aif-s3.aif.ru/images/015/653/48009b0ab04eacb7fa56ed12bfde0653.
          'https://i.pinimg.com/originals/71/11/fd/7111fdff0afab97c0d62324bccae5dae.jpg',
          'https://kto-chto-gde.ru/wp-content/uploads/2018/03/luchshie-serii-klassicheskogo-doktora-kto-7.jpg',
          'https://4.bp.blogspot.com/-mTycsZRNJjI/WWvvcU-NweI/AAAAAAAABDM/9ngBmSEFnS4FnzZjac-8GNd0mvCmquoYgCLcBGAs/s1600/Doctor%2BWho%2BMagazine%2BSpecial%2B03%2B-%2BComplete%2B6th%2BDoctor%2B%25282002%2529_page62_image8.jpg',
-         'https://fashionandphotographers.files.wordpress.com/2016/02/img_8596.jpg?w=793&h=1150',
-         'https://s2.funon.cc/img/orig/201507/28/55b776f706c68.jpg']
-pict7 = ['https://www.syl.ru/misc/i/ni/1/1/7/4/6/5/i/117465_700x525.jpg',
-         'http://factmag-images.s3.amazonaws.com/wp-content/uploads/2013/11/remembrance-11.21.2013.jpg',
-         'https://fb.ru/misc/i/gallery/71663/3143560.jpg'
+         'https://fashionandphotographers.files.wordpress.com/2016/02/img_8596.jpg?w=793&h=1150']
+pict7 = ['http://factmag-images.s3.amazonaws.com/wp-content/uploads/2013/11/remembrance-11.21.2013.jpg',
+         'https://fb.ru/misc/i/gallery/71663/3143560.jpg',
          'https://image.invaluable.com/housePhotos/Chaucer/85/637985/H4586-L161178568.jpg',
          'https://unaffiliatedcritic.com/wp-content/uploads/2017/04/The-Seventh-Doctor-Sylvester-McCoy-and-Ace-Sophie-Aldred.jpg']
 pict8 = ['https://www.soyuz.ru/public/uploads/files/6/7395782/20191113234252ee5d0a6930.jpg',
@@ -298,7 +296,8 @@ pictNo = ['https://3.bp.blogspot.com/-IFlKVxa8zqU/WI-Wi6dHfqI/AAAAAAAAA0I/eZiFzH
           'https://2.bp.blogspot.com/-8CQaOq0240w/UolFDwEG0nI/AAAAAAAACi0/wxgMLAH4cWs/s1600/DOCTOR-WHO-50TH-ANNIVERSARY_THE-DAY-OF-THE-DOCTOR_69.jpg']
 pict9 = ['https://wallup.net/wp-content/uploads/2018/10/05/907485-doctor-who-bbc-sci-fi-futuristic-series-comedy-adventure-drama-1dwho-tardis.jpg',
          'https://i.pinimg.com/originals/45/d4/d6/45d4d6968524c7e28424f4ba72b98e84.jpg',
-         'https://ic.pics.livejournal.com/debora_debora/25070693/51899/51899_original.jpg']
+         'https://cdn.quotesgram.com/img/34/42/1078952056-No_-62-The-9th-Doctor.jpg',
+         'https://i.dailymail.co.uk/i/newpix/2019/07/17/16/025129600000044D-7256875-image-a-80_1563377041268.jpg']
 pict10 = ['https://i.pinimg.com/originals/75/f8/c5/75f8c57aaaea4fb5aeefbe746e595ba5.png',
           'https://dvdbash.files.wordpress.com/2014/09/doctor-who-190-the-fires-of-pompeii-s4e02-dvdbash-14.jpg',
           'https://www.tokkoro.com/picsup/2907204-tardis-doctor-who-david-tennant-tenth-doctor___movie-wallpapers.jpg',
